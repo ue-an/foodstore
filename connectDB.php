dd72a1cae84ce53a5dc2e9ac993b264ba2980748
Urison <136285384+Urison@users.noreply.github.com>
2023-06-12 16:33:26 +0800

    Update connectDB.php

--- connectDB.php
+++ connectDB.php
@@ -1,3 +1,19 @@
 <?php
+/*
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "foodstore";
+
+$con = msqli($host,$username,$password, $dbname);
+
+if($con->connect_error){
+  die "Connection error" . $con->connect_error);
+}
+else{
+  echo "Connected to database successfully";
+}
+*/
+
 $pdo=new PDO('mysql:host=localhost;port=3306;dbname=foodstore','root', '');
 ?>
